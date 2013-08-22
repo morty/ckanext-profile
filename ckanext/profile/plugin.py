4c14f5d0ef168c57f8af1ff08c6d6b61253a801a
Sean Hammond <seanhammond@lavabit.com>
2013-08-22 16:34:19 +0200

    Initial commit

---
+++ ckanext/profile/plugin.py
@@ -0,0 +1,18 @@
+import ckan.plugins as plugins
+
+from repoze.profile.profiler import AccumulatingProfileMiddleware
+
+class ProfilePlugin(plugins.SingletonPlugin):
+    plugins.implements(plugins.IMiddleware)
+
+    def make_middleware(self, app, config):
+        #Profile the app
+        app = AccumulatingProfileMiddleware(
+            app,
+            log_filename='profiling.log',
+            cachegrind_filename='cachegrind.out',
+            discard_first_request=True,
+            flush_at_shutdown=True,
+            path='/__profile__'
+        )
+        return app
